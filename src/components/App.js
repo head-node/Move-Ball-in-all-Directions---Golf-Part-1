48d623c47c15beb49f1574ef25eb58b4ae0ac205
head-node <76886903+head-node@users.noreply.github.com>
2021-02-28 04:33:52 +0530

    "SAND"

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,76 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+const App = () => {
+  const [renderBall, setRenderBall] = useState(false);
+  // const [posi, setPosi] = useState(0);
+  const [ballPosition, setBallPosition] = useState({ left: 0, top: 0 });
+  const keyListener = (event) => {
+    switch (event.keyCode) {
+      case 39:
+        setBallPosition({
+          left: ballPosition.left + 5,
+          top: ballPosition.top
+        });
+        break;
+      case 40:
+        setBallPosition({
+          left: ballPosition.left,
+          top: ballPosition.top + 5
+        });
+        break;
+      case 37:
+        setBallPosition({
+          left: ballPosition.left - 5,
+          top: ballPosition.top
+        });
+        break;
+      case 38:
+        setBallPosition({
+          left: ballPosition.left,
+          top: ballPosition.top - 5
+        });
+        break;
+      default:
+        break;
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("keydown", keyListener);
+    console.log("Listener is", keyListener);
+
+    return () => {
+      document.removeEventListener("keydown", keyListener);
     };
-
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
-		}
+  });
+  //cleanup function
+  // WE WROTE HERE BECAUSE AFTER FIRST RENDER THE CLEAN UP WILL EXECUTE
+  // AND IT WOULD REMOVE THE PREVIOUSLY ADDED LISTENERS
+  // SO THAT WON'T BE A BUNCH OF LISTENERS ATTACH WHICH COULD HINDER PERFORMANCE OF OUR APP
+  // to add dependency
+  const buttonClickHandler = () => {
+    setRenderBall(true);
+  };
+  const renderBallOrButton = () => {
+    if (renderBall) {
+      return (
+        <div
+          className="ball"
+          style={{
+            left: ballPosition.left + "px",
+            top: ballPosition.top + "px",
+            position: "absolute"
+          }}
+        ></div>
+      );
+    } else {
+      return <button onClick={buttonClickHandler}>Click For One Ball</button>;
     }
+  };
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
-    }
+  // bind ArrowRight keydown event
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
-    }
-}
-
+  return <div className="playground">{renderBallOrButton()}</div>;
+};
 
 export default App;
